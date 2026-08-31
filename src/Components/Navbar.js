01e7f326eefad0d1a567b11f057734c07ce5c259
Khan <sohil@gmail.com>
2026-09-01 00:53:20 +0530

    Fixed Navbar transistions

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
 
-  return (
-    <nav className="navbar">
+  const [showTabs, setShowTabs] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-      {/* Website Name */}
+  useEffect(() => {
+
+    const handleScroll = () => {
+
+      // Only apply this behavior on mobile
+      if (window.innerWidth > 700) {
+        setShowTabs(true);
+        return;
+      }
+
+      const currentScrollY = window.scrollY;
+
+      // At top
+      if (currentScrollY <= 10) {
+        setShowTabs(true);
+      }
+
+      // Scroll DOWN
+      else if (currentScrollY > lastScrollY) {
+        setShowTabs(false);
+      }
+
+      // Scroll UP
+      else if (currentScrollY < lastScrollY) {
+        setShowTabs(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll, {
+      passive: true
+    });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+  }, [lastScrollY]);
+
+
+  return (
+    <nav
+      className={`navbar ${showTabs
+          ? "navbar-tabs-visible"
+          : "navbar-tabs-hidden"
+        }`}
+    >
+
+      {/* =================================================
+                WEBSITE NAME
+            ================================================= */}
 
       <div className="navbar-logo">
+
         <Link to="/">
           Hussain Marble & Stones
         </Link>
+
       </div>
 
 
-      {/* Navigation Tabs */}
+      {/* =================================================
+                MOBILE TAB BOX
+            ================================================= */}
 
-      <div className="navbar-links">
+      <div
+        className={`navbar-links ${showTabs
+            ? "tabs-visible"
+            : "tabs-hidden"
+          }`}
+      >
 
         <Link to="/">
           Home
